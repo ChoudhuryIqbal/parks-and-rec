a7fe9e8f968025c10387baeaf4d811a9eab2e7c0
OZ <oxz3@psu.edu>
2018-11-19 23:03:44 -0800

    Excluded classes from testing in the pom.xml file. Added exclusion annotations for main and test methods.

--- src/main/java/ParksAndRecApplication.java
+++ src/main/java/ParksAndRecApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan (basePackages= { "edu.psu.sweng894.group7" })
 @SpringBootApplication
 @EntityScan(basePackages = {"edu.psu.sweng894.group7.datastore.entity"})
+@lombok.Generated
 public class ParksAndRecApplication {
 		public static void main(String[] args) {
 			SpringApplication.run(ParksAndRecApplication.class, args);
